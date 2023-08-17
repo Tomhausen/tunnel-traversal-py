a81e0985d5fe5a87a6014d59f3de34e3e69026ee
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2023-08-17 10:36:27 +0100

    w1 hack

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class SpriteKind:
 # variables
 gravity = 8
 game_speed = 50
-min_spawn_gap = 50
+min_spawn_gap = 25 #
 camera_offset = 64
 last_spawn_x: number = None
 camera_right = scene.camera_property(CameraProperty.RIGHT)
@@ -13,13 +13,17 @@ camera_right = scene.camera_property(CameraProperty.RIGHT)
 # ground
 bottom = sprites.create(assets.image("bottom"), SpriteKind.ground)
 bottom.bottom = 120
-bottom.z = 10
 bottom.vx = game_speed
+top = sprites.create(assets.image("bottom"), SpriteKind.player) #
+top.image.fill(2) #
+top.top = 0 #
+top.vx = game_speed #
 
 # player
 bot = sprites.create(assets.image("bot"), SpriteKind.player)
 bot.set_position(16, 80)
 bot.vx = game_speed
+bot.z = -1
 
 # setup
 scene.set_background_color(1)
@@ -27,7 +31,16 @@ scene.set_background_color(1)
 def jump():
     if bot.overlaps_with(bottom):
         bot.vy = -175
+    if bot.overlaps_with(top): #
+        bot.vy = 175 #
 controller.A.on_event(ControllerButtonEvent.PRESSED, jump)
+
+def switch_side(): #
+    if bot.overlaps_with(bottom):
+        bot.vy = -200
+    if bot.overlaps_with(top):
+        bot.vy = 200
+controller.B.on_event(ControllerButtonEvent.PRESSED, switch_side) #
 
 def hit_obstacle(bot, obstacle):
     game.over(True)
@@ -37,6 +50,9 @@ def spawn_obstacle():
     global last_spawn_x
     obstacle = sprites.create(assets.image("blue obstacle"), SpriteKind.enemy)
     obstacle.bottom = 110
+    if randint(1, 2) == 1: #
+        obstacle.set_image(assets.image("red obstacle")) #
+        obstacle.top = 10 #
     obstacle.left = camera_right - 5
     last_spawn_x = obstacle.left
     obstacle.set_flag(SpriteFlag.AUTO_DESTROY, True)
@@ -48,7 +64,11 @@ def move_camera():
     camera_right = scene.camera_property(CameraProperty.RIGHT)
 
 def y_movement():
-    bot.vy += gravity
+    # bot.vy += gravity
+    if bot.y > 60: #
+        bot.vy += gravity #
+    else: #
+        bot.vy -= gravity #
 
 def hit_floor(floor):
     if bot.overlaps_with(floor):
@@ -60,9 +80,10 @@ def hit_floor(floor):
 def tick():
     move_camera()
     if camera_right - last_spawn_x > min_spawn_gap:
-        if randint(1, 30) == 1:
+        if randint(1, 15) == 1:
             spawn_obstacle()
     y_movement()
+    hit_floor(top) # 
     hit_floor(bottom)
     info.change_score_by(1)
 game.on_update(tick)
