56748286429726cc708d06d1dcd3b252018df4b6
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2023-08-17 11:08:39 +0100

    Restore '8/17/2023, 10:51:43 AM w2 hack flip'

--- main.ts
+++ main.ts
@@ -5,27 +5,70 @@ namespace SpriteKind {
 //  variables
 let gravity = 8
 let game_speed = 50
-let min_spawn_gap = 50
+let min_spawn_gap = 25
+// 
 let camera_offset = 64
 let last_spawn_x : number = null
 let camera_right = scene.cameraProperty(CameraProperty.Right)
 //  ground
 let bottom = sprites.create(assets.image`bottom`, SpriteKind.ground)
 bottom.bottom = 120
-bottom.z = 10
 bottom.vx = game_speed
+let top = sprites.create(assets.image`bottom`, SpriteKind.Player)
+// 
+top.image.fill(2)
+// 
+top.top = 0
+// 
+top.vx = game_speed
+// 
 //  player
 let bot = sprites.create(assets.image`bot`, SpriteKind.Player)
 bot.setPosition(16, 80)
 bot.vx = game_speed
+bot.z = -1
 //  setup
 scene.setBackgroundColor(1)
 controller.A.onEvent(ControllerButtonEvent.Pressed, function jump() {
     if (bot.overlapsWith(bottom)) {
-        bot.vy = -175
+        bot.vy = -150
+        timer.background(function clockwise_flip() {
+            bot.y -= 5
+            while (!bot.overlapsWith(bottom)) {
+                transformSprites.changeRotation(bot, 10)
+                pause(50)
+            }
+            transformSprites.rotateSprite(bot, 90)
+        })
+    }
+    
+    if (bot.overlapsWith(top)) {
+        // 
+        bot.vy = 150
+        // 
+        timer.background(function anticlockwise_flip() {
+            bot.y += 5
+            while (!bot.overlapsWith(top)) {
+                transformSprites.changeRotation(bot, -10)
+                pause(50)
+            }
+            transformSprites.rotateSprite(bot, 90)
+        })
     }
     
 })
+controller.B.onEvent(ControllerButtonEvent.Pressed, function switch_side() {
+    // 
+    if (bot.overlapsWith(bottom)) {
+        bot.vy = -200
+    }
+    
+    if (bot.overlapsWith(top)) {
+        bot.vy = 200
+    }
+    
+})
+// 
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function hit_obstacle(bot: Sprite, obstacle: Sprite) {
     game.over(true)
 })
@@ -33,6 +76,14 @@ function spawn_obstacle() {
     
     let obstacle = sprites.create(assets.image`blue obstacle`, SpriteKind.Enemy)
     obstacle.bottom = 110
+    if (randint(1, 2) == 1) {
+        // 
+        obstacle.setImage(assets.image`red obstacle`)
+        // 
+        obstacle.top = 10
+    }
+    
+    // 
     obstacle.left = camera_right - 5
     last_spawn_x = obstacle.left
     obstacle.setFlag(SpriteFlag.AutoDestroy, true)
@@ -46,9 +97,19 @@ function move_camera() {
 }
 
 function y_movement() {
-    bot.vy += gravity
+    //  bot.vy += gravity
+    if (bot.y > 60) {
+        // 
+        bot.vy += gravity
+    } else {
+        // 
+        // 
+        bot.vy -= gravity
+    }
+    
 }
 
+// 
 function hit_floor(floor: any) {
     if (bot.overlapsWith(floor)) {
         while (bot.overlapsWith(floor)) {
@@ -63,13 +124,15 @@ function hit_floor(floor: any) {
 game.onUpdate(function tick() {
     move_camera()
     if (camera_right - last_spawn_x > min_spawn_gap) {
-        if (randint(1, 30) == 1) {
+        if (randint(1, 15) == 1) {
             spawn_obstacle()
         }
         
     }
     
     y_movement()
+    hit_floor(top)
+    //  
     hit_floor(bottom)
     info.changeScoreBy(1)
 })
